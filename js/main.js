61ad804ea5ba2f3ae6d5b3d9338cabe6fa46dc23
Celest1alSolace <anvarjonov.m777@gmail.com>
2024-07-27 14:59:52 +0500

    Finishing

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let elModalWrapper = document.querySelector(".modal")
+let elOpen = document.querySelector(".open-icon")
+let elClose = document.querySelector(".close-icon")
+let elSun = document.querySelector(".dark-item")
+
+function handleMenuIconOpen(){
+    elModalWrapper.classList.add("show")
+    elClose.classList.add("close-show")
+    elOpen.classList.add("hide")
+}
+function handleMenuIconClose(){
+    elModalWrapper.classList.remove("show")
+    elClose.classList.remove("close-show")
+    elOpen.classList.remove("hide")
+}
+function CloseList(){
+    elModalWrapper.classList.remove("show")
+    elClose.classList.remove("close-show")
+    elOpen.classList.remove("hide")
+}
+function darkMode(){
+    elSun.classList.toggle("right")
+    document.elSun.classList.toggle("dark")
+}
